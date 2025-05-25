c57a18bf453ea05a2e96b0d038668fb4e504f341
Heba Naba <hebaanbaa@gmail.com>
2025-05-25 12:41:40 +0400

    changed

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,10 +1,9 @@
-export const environment = {
-    apiKey: "AIzaSyB132q5WSNcLcoL7sFr4AITgFL2YUD3JWc",
-    authDomain: "sideeffectsapp.firebaseapp.com",
-    databaseURL: "https://sideeffectsapp-default-rtdb.firebaseio.com",
-    projectId: "sideeffectsapp",
-    storageBucket: "sideeffectsapp.firebasestorage.app",
-    messagingSenderId: "628029335425",
-    appId: "1:628029335425:web:9e47b02e1c3bf9ec55aa80",
-    measurementId: "G-786VKV77F3"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyAQTjdzso3IVsUVXpE-o5NUuZAw81ug8Nk",
+  authDomain: "smart-fridge-d2345.firebaseapp.com",
+  projectId: "smart-fridge-d2345",
+  storageBucket: "smart-fridge-d2345.firebasestorage.app",
+  messagingSenderId: "754008605260",
+  appId: "1:754008605260:web:83b6ad68e3442fec67eae9",
+  measurementId: "G-PVD2YRQQ9H"
+};
