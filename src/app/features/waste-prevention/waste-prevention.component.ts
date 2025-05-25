dbb12b88361b8f45c097446ff8efd9b36df4b391
Heba Naba <hebaanbaa@gmail.com>
2025-05-25 12:37:20 +0400

    changed

--- src/app/features/waste-prevention/waste-prevention.component.ts
+++ src/app/features/waste-prevention/waste-prevention.component.ts
@@ -1,12 +1,1 @@
-import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-waste-prevention',
-  standalone: true,
-  imports: [],
-  templateUrl: './waste-prevention.component.html',
-  styleUrl: './waste-prevention.component.css'
-})
-export class WastePreventionComponent {
-
-}
